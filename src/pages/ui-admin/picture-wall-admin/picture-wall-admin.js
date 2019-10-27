7b8f2b9c3de00a8dfa9eff5b365fe2733184a2e7
yjh <1057443070@qq.com>
2019-10-27 11:05:34 +0800

    第二次提交--图片画廊3

--- src/pages/ui-admin/picture-wall-admin/picture-wall-admin.js
+++ src/pages/ui-admin/picture-wall-admin/picture-wall-admin.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react'
-import {Card, Col, Row} from "antd"
+import {Card, Col, Modal, Row} from "antd"
 const { Meta } = Card
 export default class PictureWallAdmin extends Component{
 
+    state = {
+        visible: false
+    }
+
+
+    openGallery = (item) => {
+        this.setState({
+            visible: true,
+            // 如果没法确定初值状态，可以先不定义，
+            //先着手实现，最后定义初值状态
+            currentImg: '/gallery/'+ item,
+        })
+    }
 
     render() {
         const imgs = [
@@ -17,7 +30,8 @@ export default class PictureWallAdmin extends Component{
                 hoverable
                 key={index}
                 style={{ width: 240, marginBottom: 15 }}
-                cover={ <img src={'/gallery/' + item} alt={index} /> }
+                cover={ <img src={'/gallery/' + item} alt={index}   /> }
+                onClick={ ()=> this.openGallery(item) }
             >
                 <Meta title='图片画廊' description={item}/>
             </Card>
@@ -41,6 +55,16 @@ export default class PictureWallAdmin extends Component{
                         {imgList[4]}
                     </Col>
                 </Row>
+                <Modal
+                    title="图片画廊"
+                    visible={this.state.visible}
+                    onCancel={ () => this.setState({visible:false}) }   /* 必须含有隐藏对话框的回调函数   */
+                    footer={null}
+                    width={600}    /*  设置对话框的宽度     */
+                    height={800}
+                >
+                    <img src={this.state.currentImg} alt='' style={{width:'100%'}}/>   {/* 图片占对话框的百分比 */}
+                </Modal>
             </div>
         )
     }
