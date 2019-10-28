376853555d8a545dc1dcb02a9e27eb52545aca12
yujihua <yujh85@chinaunicom.cn>
2019-10-28 13:49:05 +0800

    第一次开发----组件的使用

--- src/pages/form-admin/login-admin/login-admin.js
+++ src/pages/form-admin/login-admin/login-admin.js
@@ -1,19 +1,23 @@
 import React, {Component} from 'react'
 import {Card, Form, Icon, Input, Button, Checkbox} from "antd"
 
+import '../form-admin.less'
 
+// 是否有错误
 function hasErrors(fieldsError) {
     return Object.keys(fieldsError).some(field => fieldsError[field])
 }
 
 class LoginAdmin extends Component{
 
+
     componentDidMount() {
         // To disabled submit button at the beginning.
         this.props.form.validateFields()
     }
 
-    handleSubmit = e => {
+
+    handleSubmit1 = e => {
         e.preventDefault()
         this.props.form.validateFields((err, values) => {
             if (!err) {
@@ -22,9 +26,9 @@ class LoginAdmin extends Component{
         })
     }
 
-    // 第二个card
+    // 第二个card的提交
     // 自动提交表单
-    handleSubmit = e => {
+    handleSubmit2 = e => {
         // 如果使用默认提交，则需要阻止该事件的默认行为
         e.preventDefault()
         this.props.form.validateFields((err, values) => {
@@ -39,7 +43,8 @@ class LoginAdmin extends Component{
         // Only show error after a field is touched.
         const usernameError = isFieldTouched('username') && getFieldError('username')
         const passwordError = isFieldTouched('password') && getFieldError('password')
-        // 对form表单进行设置
+        // 对form表单布局进行设置
+        // 响应式布局
         const formItemLayout = {
             labelCol: {
                 xs: { span: 24 },
@@ -49,11 +54,12 @@ class LoginAdmin extends Component{
                 xs: { span: 15 },
                 sm: { span: 16 },
             },
-        };
+        }
+
         return (
             <div>
-                <Card title='内联(行内)登录表单'>
-                    <Form layout="inline" onSubmit={this.handleSubmit}>
+                <Card title='内联(行内)登录表单' className='card-wrap'>
+                    <Form layout="inline" onSubmit={this.handleSubmit1}>
                         <Form.Item validateStatus={usernameError ? 'error' : ''} help={usernameError || ''}>
                             {getFieldDecorator('username', {
                                 rules: [{ required: true, message: 'Please input your username!' }],
@@ -82,8 +88,10 @@ class LoginAdmin extends Component{
                         </Form.Item>
                     </Form>
                 </Card>
-                <Card title='普通（水平）的登录框'>
-                    <Form onSubmit={this.handleSubmit} className="login-form" {...formItemLayout}>
+
+
+                <Card title='普通（水平）的登录框' className='card-wrap'>
+                    <Form onSubmit={this.handleSubmit2} className="login-form" {...formItemLayout}>
                         <Form.Item>
                             {getFieldDecorator('username', {
                                 rules: [{ required: true, message: '用户名必须输入！' }],
@@ -105,6 +113,7 @@ class LoginAdmin extends Component{
                                 />,
                             )}
                         </Form.Item>
+
                         <Form.Item>
                             {getFieldDecorator('remember', {
                                 valuePropName: 'checked',
