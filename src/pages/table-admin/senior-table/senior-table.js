f609775433af9f17fb0f5eb2d315621b2308bc49
yujihua <yujh85@chinaunicom.cn>
2019-11-01 17:59:00 +0800

    第4次开发----高级表格2

--- src/pages/table-admin/senior-table/senior-table.js
+++ src/pages/table-admin/senior-table/senior-table.js
@@ -1,5 +1,34 @@
 import React, {Component} from 'react'
-import {Card} from "antd";
+import {Card, Table} from "antd";
+
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        width: 150,
+    },
+    {
+        title: 'Age',
+        dataIndex: 'age',
+        width: 150,
+    },
+    {
+        title: 'Address',
+        dataIndex: 'address',
+    },
+];
+
+const data = [];
+for (let i = 0; i < 100; i++) {
+    data.push({
+        key: i,
+        name: `Edward King ${i}`,
+        age: 32,
+        address: `London, Park Lane no. ${i}`,
+    });
+}
+
 
 export default class SeniorTable extends Component{
 
@@ -7,7 +36,7 @@ export default class SeniorTable extends Component{
         return (
             <div>
                 <Card title='高级表格---固定表头' className='card-wrap'>
-
+                    <Table columns={columns} dataSource={data} pagination={{ pageSize: 50 }} scroll={{ y: 240 }} />
                 </Card>
                 <Card title='高级表格---固定左侧' className='card-wrap'>
 
